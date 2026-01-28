0e2c710cfaeb57d16034c3bdc44a0729d308d9a8
Bruno Antunes <premiumbruno9@gmail.com>
2026-01-28 11:40:20 -0300

    Replace blueprint type from enum to string.

--- internal/data/blueprint.go
+++ internal/data/blueprint.go
@@ -1,13 +1,11 @@
 package data
 
-import "singularity/internal/enum"
-
 type Blueprint struct {
-	Id          string             `json:"id" bson:"id" toml:"id"`
-	Name        string             `json:"name" bson:"name" toml:"name"`
-	Type        enum.BlueprintType `json:"blueprint_type" bson:"type" toml:"type"`
-	Volumes     []Volume           `json:"volumes" bson:"volumes" toml:"volumes"`
-	Environment map[string]string  `json:"environment" bson:"environment" toml:"environment"`
+	Id          string            `json:"id" bson:"id" toml:"id"`
+	Name        string            `json:"name" bson:"name" toml:"name"`
+	Type        string            `json:"type" toml:"type"`
+	Volumes     []Volume          `json:"volumes" bson:"volumes" toml:"volumes"`
+	Environment map[string]string `json:"environment" bson:"environment" toml:"environment"`
 }
 
 type Volume struct {
